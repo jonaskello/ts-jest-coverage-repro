0da032b4c9ad4e9557d003862fcdb31fe87d536b
Jonas Kello <jonas.kello@divid.se>
2017-03-25 14:48:24 +0100

    Add code coverage config

--- test/react.test.tsx
+++ test/react.test.tsx
@@ -1,13 +1,10 @@
-
-// Link.react-test.js
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 
-test('Link changes the class when hovered', () => {
+test('Test react component', () => {
   const component = renderer.create(
     <div>Testing</div>
   );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
-
 });
